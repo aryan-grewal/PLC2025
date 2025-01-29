3a0e66435f6541a4f79d8297e1ff3010672a958d
Aryanjot Grewal <aryanzgrewal@gmail.com>
2025-01-29 20:36:25 +0000

    More generalised version of func_as_arg.js

--- lab2-functionalprimer/javascript/func_as_arg.js
+++ lab2-functionalprimer/javascript/func_as_arg.js
@@ -1,4 +1,4 @@
-//Create list of ints from 1 to 5, Haskell equivalent [1..5]
+/* //Create list of ints from 1 to 5, Haskell equivalent [1..5]
 function arrFunc(){
     let arr = [];
     for (let i = 1; i<=5; i++) {
@@ -21,4 +21,49 @@ function applicatorFunc(inpFunc, s){
 }
 
 let x = applicatorFunc(arrFunc, 's');
-console.log(x);
+console.log(x); */
+
+const readline = require('readline');
+// Generalized version
+function arrFunc(start, end){
+    let arr = [];
+    for (let i = start; i <= end; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+
+function applicatorFunc(arr, operation){
+    let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    if (operation === 'sum') {
+        return sum;
+    } else if (operation === 'avg') {
+        return sum / arr.length;
+    } else {
+        throw new Error("Invalid operation. Use 'sum' or 'avg'.");
+    }
+}
+
+// Setup readline interface for user input in Node.js
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Get user input
+rl.question('Enter the start number: ', (start) => {
+    rl.question('Enter the end number: ', (end) => {
+        rl.question('Enter "sum" for the sum or "avg" for the average: ', (operation) => {
+            // Convert input to integers
+            start = parseInt(start);
+            end = parseInt(end);
+
+            let arr = arrFunc(start, end)
+            let result = applicatorFunc(arr, operation);
+            console.log(result);
+
+            rl.close();
+        });
+    });
+});
